ae9510d9cd274b7af134dd3831c9f7023844e228
garry <guyu545kou@gmail.com>
2023-09-03 11:52:03 +0800

    update

---
+++ test/src/main/java/org/example/MyTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.io.IOException;
+
+/**
+ * @author guyu06
+ * @date 2023/9/3 11:48
+ */
+public class MyTest {
+
+    public static void main(String[] args) throws IOException {
+        A obj = new B();
+        System.in.read();
+        System.out.println(obj);
+    }
+}
